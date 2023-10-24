661cb9d007512d64cf1c8d037f9a311028817bb9
Alex N <ichiv.0zv@gmail.com>
2023-10-24 15:32:32 +0100

    Kick script

--- Assets/Scripts/KickThings.cs
+++ Assets/Scripts/KickThings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,41 @@ public class KickThings : MonoBehaviour
 {
     public GameObject pressE;
     public GameObject player;
+    public GameObject target;
     public float kickForce;
+    private bool _kickReady;
+
+    private void Start()
+    {
+        _kickReady = false;
+    }
+
+    private void Update()
+    {
+        if (_kickReady & Input.GetKeyDown(KeyCode.E))
+        {
+            target.gameObject.GetComponent<Rigidbody>().AddForce((-player.transform.forward+player.transform.up) * kickForce, ForceMode.Impulse);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("CanKick"))
         {
+            _kickReady = true;
             pressE.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                other.gameObject.GetComponent<Rigidbody>().AddForce(-player.transform.forward * kickForce, ForceMode.Impulse);
-                Debug.Log("Kick");
-            }
+            target = other.gameObject;
         }
         else
         {
             pressE.SetActive(false);
+            _kickReady = false;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pressE.SetActive(false);
+        _kickReady = false;
     }
 }
